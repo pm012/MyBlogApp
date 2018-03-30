5d451fb0520bd13df9ea32dde17912717f8c4f7f
Sergii Kroshka <SKroshka@luxoft.com>
2018-03-30 18:57:20 +0300

    Improved update form - the form is filled with data. Changed button input type to match thymeleaf. Added method find one to fill in the form on index.html

--- Blog/src/main/resources/static/js/main.js
+++ Blog/src/main/resources/static/js/main.js
@@ -5,7 +5,16 @@
 $(document).ready(function(){
 	
 	$('.table .eBtn').on('click',function(event){
+		event.preventDefault();
+		var href = $(this).attr('href');
 		
+		$.get(href,function(user,status){
+			$('.myForm #id').val(user.id);
+			$('.myForm #name').val(user.name);
+			$('.myForm #lastName').val(user.lastName);
+			$('.myForm #email').val(user.email);
+			
+		});
 		$('.myForm #exampleModal').modal();
 	});
 });
